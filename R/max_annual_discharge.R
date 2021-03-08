d8d3c57274fae3a930fe0fcd73fd5b5c3b8418c5
claudiapflores <cpflores@bren.ucsb.edu>
2021-03-08 15:07:05 -0800

    Added data for recurrence_interval_prob function

---
+++ R/max_annual_discharge.R
@@ -0,0 +1,15 @@
+#' max_annual_discharge
+#'
+#' data for recurrence_interval_prob function
+#'
+#' @format A data frame with 10 rows and 4 columns
+#' \describe{
+#' \item {R}{ranking value}
+#' \item {max_annual_discharge_m3_s}{maximum annual discharge (m^3/s)}
+#' \item {year}{year when maximum annual discharge value was recorded}
+#' \item {Y}{the total number of records for maximum annual discharge in the data frame}
+#' }
+#'
+#' @author Claudia Flores
+#'
+"max_annual_discharge"
