df5b874e85f0339f2d0e59dc9e6b2c577907cdd0
marianoviz <mviz@bren.ucsb.edu>
2021-03-09 11:19:10 -0300

    Added data + data documentation for diatom_df

---
+++ R/diatom_df.R
@@ -0,0 +1,14 @@
+#' Diatom df
+#'
+#' Data testing the function predominant_diatom
+#'
+#' @format A data frame with 100 rows and 4 columns
+#' \describe{
+#' \item{p_conc}{Phosphate (PO4) concentration (mg/L)}
+#' \item{si_conc}{Silicate (SiO2) concentration (mg/L)}
+#' \item{temp}{Lake water temperature (C)}
+#' \item{predominant_diatom}{Output of the predominant_diatom() function}
+#' }
+#' @author Mariano Viz
+#'
+"diatom_df"
